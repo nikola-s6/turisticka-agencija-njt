3808f21322e43ccb99956cc14618d66e39008e13
nikolas <nikola.stojilkovic6@gmail.com>
2023-06-01 00:10:39 +0200

    Izmenjena main metoda za pokretanje aplikacije.

--- TuristickaAgencijaClient/src/main/java/rs/ac/bg/fon/Main.java
+++ TuristickaAgencijaClient/src/main/java/rs/ac/bg/fon/Main.java
@@ -1,7 +1,9 @@
 package rs.ac.bg.fon;
 
+import rs.ac.bg.fon.view.coordinator.Coordinator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Coordinator.getInstance().openLoginForm();
     }
 }
