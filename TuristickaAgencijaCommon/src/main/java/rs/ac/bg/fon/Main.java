8dadfa4be7d7862d1bb56b990db9d1049ff36461
nikolas <nikola.stojilkovic6@gmail.com>
2023-05-31 21:13:30 +0200

    Napravljene klase Request i Response za komunikaciju.

--- TuristickaAgencijaCommon/src/main/java/rs/ac/bg/fon/Main.java
+++
@@ -1,7 +0,0 @@
-package rs.ac.bg.fon;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ TuristickaAgencijaCommon/src/main/java/rs/ac/bg/fon/communication/Request.java
@@ -0,0 +1,32 @@
+package rs.ac.bg.fon.communication;
+
+import java.io.Serializable;
+
+public class Request implements Serializable {
+    private Operation operation;
+    private Object argument;
+
+    public Request() {
+    }
+
+    public Request(Operation operation, Object argument) {
+        this.operation = operation;
+        this.argument = argument;
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    public Object getArgument() {
+        return argument;
+    }
+
+    public void setArgument(Object argument) {
+        this.argument = argument;
+    }
+}
