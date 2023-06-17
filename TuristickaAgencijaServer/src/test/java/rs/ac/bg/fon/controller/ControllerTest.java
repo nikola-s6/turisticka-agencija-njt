5ba7bfa43496ff312e9cd4d23a0a633ceb0ec629
nikolas <nikola.stojilkovic6@gmail.com>
2023-06-17 20:22:47 +0200

    Testirane sistmenske operacije za ucitavanje liste gradova, putnika, putovanja i termina.

--- TuristickaAgencijaServer/src/test/java/rs/ac/bg/fon/controller/ControllerTest.java
+++ TuristickaAgencijaServer/src/test/java/rs/ac/bg/fon/controller/ControllerTest.java
@@ -737,4 +737,116 @@ class ControllerTest {
         controller.obrisiPutnika(putnik);
     }
 
+    @Test
+    void ucitajListuGradova() throws Exception {
+        List<Grad> gradovi = controller.ucitajListuGradova();
+        assertTrue(gradovi.size() == 2);
+    }
+
+    @Test
+    void ucitajListuPutnika() throws Exception {
+        Putnik putnik1 = new Putnik();
+        putnik1.setIme("Nikola");
+        putnik1.setPrezime("Stojilkovic");
+        putnik1.setPol(Pol.MUSKI);
+        putnik1.setEmail("nikola.stojilkovic6@gmail.com");
+        putnik1.setBrojTelefona("0601111111");
+        putnik1.setSifra("nikola123");
+        putnik1 = controller.zapamtiPutnika(putnik1);
+
+        Putnik putnik2 = new Putnik();
+        putnik2.setIme("Marko");
+        putnik2.setPrezime("Nikolic");
+        putnik2.setPol(Pol.MUSKI);
+        putnik2.setEmail("marko.nikolic@gmail.com");
+        putnik2.setBrojTelefona("0602222222");
+        putnik2.setSifra("marko123");
+        putnik2 = controller.zapamtiPutnika(putnik2);
+
+        List<Putnik> putnici = controller.ucitajListuPutnika();
+
+        assertTrue(putnici.size() == 2);
+        assertEquals(putnik1.getPutnikID(), putnici.get(0).getPutnikID());
+        assertEquals(putnik2.getPutnikID(), putnici.get(1).getPutnikID());
+
+        controller.obrisiPutnika(putnik1);
+        controller.obrisiPutnika(putnik2);
+    }
+
+    @Test
+    void ucitajListuPutovanja() throws Exception {
+        Putnik putnik = new Putnik();
+        putnik.setIme("Nikola");
+        putnik.setPrezime("Stojilkovic");
+        putnik.setPol(Pol.MUSKI);
+        putnik.setEmail("nikola.stojilkovic6@gmail.com");
+        putnik.setBrojTelefona("0601111111");
+        putnik.setSifra("nikola123");
+        putnik = controller.zapamtiPutnika(putnik);
+
+        Grad g1 = new Grad();
+        g1.setGradID(28);
+        Grad g2 = new Grad();
+        g2.setGradID(29);
+        Putovanje putovanje = new Putovanje();
+        putovanje.setPocetniGrad(g1);
+        putovanje.setKrajnjiGrad(g2);
+        putovanje.setPrevoz(Prevoz.AVION);
+        putovanje.setSmestaj(Smestaj.HOTEL);
+        putovanje.setPonuda(Ponuda.POLU_PANSION);
+        putovanje.setKratakOpis("Opis putovanja");
+        putovanje.setNaziv("Obilazak Rima");
+
+        putovanje = controller.zapamtiPutovanje(putovanje);
+
+        List<Putovanje> putovanja = controller.ucitajListuPutovanja();
+
+        assertTrue(putovanja.size() > 0);
+        assertEquals(putovanje.getPutovanjeID(), putovanja.get(putovanja.size() - 1).getPutovanjeID());
+
+        controller.obrisiPutnika(putnik);
+    }
+
+    @Test
+    void ucitajListuTermina() throws Exception {
+        Grad g1 = new Grad();
+        g1.setGradID(28);
+        Grad g2 = new Grad();
+        g2.setGradID(29);
+        Putovanje putovanje = new Putovanje();
+        putovanje.setPocetniGrad(g1);
+        putovanje.setKrajnjiGrad(g2);
+        putovanje.setPrevoz(Prevoz.AVION);
+        putovanje.setSmestaj(Smestaj.HOTEL);
+        putovanje.setPonuda(Ponuda.POLU_PANSION);
+        putovanje.setKratakOpis("Opis putovanja");
+        putovanje.setNaziv("Obilazak Rima");
+
+        putovanje = controller.zapamtiPutovanje(putovanje);
+
+        Termin termin1 = new Termin();
+        termin1.setCena(12000.00);
+        termin1.setDatumPolaska(new Date(2023, Calendar.OCTOBER, 13));
+        termin1.setDatumPovratka(new Date(2023, Calendar.OCTOBER, 20));
+        termin1.setPutovanje(putovanje);
+
+        Termin termin2 = new Termin();
+        termin2.setCena(13000.00);
+        termin2.setDatumPolaska(new Date(2023, Calendar.OCTOBER, 19));
+        termin2.setDatumPovratka(new Date(2023, Calendar.OCTOBER, 26));
+        termin2.setPutovanje(putovanje);
+
+        putovanje.setTermini(List.of(termin1, termin2));
+
+        putovanje = controller.zapamtiPutovanje(putovanje);
+        termin1.setTerminID(putovanje.getTermini().get(0).getTerminID());
+        termin2.setTerminID(putovanje.getTermini().get(1).getTerminID());
+
+        List<Termin> termini = controller.ucitajListuTermina(putovanje);
+
+        assertTrue(termini.size() == 2);
+        assertEquals(termin1.getTerminID(), termini.get(0).getTerminID());
+        assertEquals(termin2.getTerminID(), termini.get(1).getTerminID());
+
+    }
 }
