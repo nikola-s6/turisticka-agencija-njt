85a9f473a938c89e7c7cc13f4afa412892e1d81b
nikolas <nikola.stojilkovic6@gmail.com>
2023-06-21 22:24:35 +0200

    Dodati testovi za domensku klasu Grad.

--- TuristickaAgencijaCommon/src/test/java/rs/ac/bg/fon/domain/GradTest.java
+++ TuristickaAgencijaCommon/src/test/java/rs/ac/bg/fon/domain/GradTest.java
@@ -20,15 +20,50 @@ class GradTest {
     }
 
     @Test
+    void setGradIDKadaJeNula(){
+        assertThrows(IllegalArgumentException.class, () -> grad.setGradID(0));
+    }
+
+    @Test
     void setGradID() {
         grad.setGradID(1);
         assertEquals(1, grad.getGradID());
     }
 
     @Test
+    void setNazivNull(){
+        assertThrows(NullPointerException.class, ()-> grad.setNaziv(null));
+    }
+
+    @Test
+    void setNazivPrazanString(){
+        assertThrows(IllegalArgumentException.class, ()-> grad.setNaziv(""));
+    }
+
+    @Test
+    void setNazviNijeVelikoPrvoSlovo(){
+        assertThrows(IllegalArgumentException.class, ()-> grad.setNaziv("beograd"));
+    }
+
+    @Test
     void setNaziv() {
         grad.setNaziv("Beograd");
         assertEquals("Beograd", grad.getNaziv());
+    }
+
+    @Test
+    void setDrzavaNull(){
+        assertThrows(NullPointerException.class, ()-> grad.setDrzava(null));
+    }
+
+    @Test
+    void setDrzavaPrazanString(){
+        assertThrows(IllegalArgumentException.class, ()-> grad.setDrzava(""));
+    }
+
+    @Test
+    void setDrzavaNijeVelikoPrvoSlovo(){
+        assertThrows(IllegalArgumentException.class, ()-> grad.setDrzava("srbija"));
     }
 
     @Test
